70823f66ff74f8c66d48a3448fd10cb93850c448
Austin Ouyang <AustinOuyang@Austins-MacBook-Air.local>
2015-05-11 15:27:17 -0700

    Added images for readme and modified setup_ipython so it utilizes more memory on startup

--- Devs/Spark/setup_ipython.sh
+++ Devs/Spark/setup_ipython.sh
@@ -9,5 +9,10 @@ echo -e "\nexport AWS_ACCESS_KEY_ID=$1" | cat >> ~/.profile
 echo -e "\nexport AWS_SECRET_ACCESS_KEY=$2" | cat >> ~/.profile
 . ~/.profile
 
+MEMINFO=($(free -m | sed -n '2p' | sed -e "s/[[:space:]]\+/ /g"))
+TOTMEM=${MEMINFO[1]}
+EXECMEM=$(echo "0.70 * $TOTMEM" | bc -l)
+
 tmux new-session -s ipython_notebook -n bash -d
-tmux send-keys -t ipython_notebook 'PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777" pyspark --master spark://'"$(hostname)"':7077' C-m
+
+tmux send-keys -t ipython_notebook 'PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777" pyspark --master spark://'$(hostname)':7077 --executor-memory '${EXECMEM%.*}'M' C-m
