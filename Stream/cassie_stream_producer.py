324520f37ec389544e7fd19733ad99fde9d0d399
Ubuntu <ubuntu@ip-172-31-32-150.us-west-2.compute.internal>
2015-05-15 17:14:23 +0000

    modified cassandra stream producer to playback in realtime and in order

--- Stream/cassie_stream_producer.py
+++ Stream/cassie_stream_producer.py
@@ -13,7 +13,7 @@ session = cluster.connect()
 session.execute(
     """
     CREATE KEYSPACE IF NOT EXISTS execed
-    WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 3 }
+    WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 2 }
     """
 )
 
@@ -31,10 +31,24 @@ session.execute(
 f = open('data/sorted_fashion.json','r')
 data = f.readlines()
 
-count = 0
+# compute deltas in record timestamp
+tdeltas = None
+max_tdelta = 20
+for curr_data in data:
+    record = json.loads(curr_data)
+    if tdeltas is None:
+        prev_ts = record["time"]
+        tdeltas = []
+    else:
+        tdelta = (record["time"] - prev_ts)/1000.0 
+        if tdelta < max_tdelta:
+            tdeltas.append(tdelta)
+        prev_ts = record["time"]
 
+# playback records in order along and repeat when finished
+count = 0
 while True:
-    curr_data = random.choice(data)
+    curr_data = data[count % len(data)]
     record = json.loads(curr_data)
 
     td_user_last_action_time = record["user_last_action_time"]-record["time"]
@@ -42,7 +56,7 @@ while True:
 
     ts_now = datetime.now()
     ts = long((ts_now-datetime.fromtimestamp(0)).total_seconds()*1000)
-    print ts
+    print("%s %s" % (ts_now, "UTC"))
     new_time = ts
     new_user_last_action_time = ts + td_user_last_action_time
     new_registration_time = ts + td_registration_time
@@ -51,13 +65,14 @@ while True:
     curr_data = re.sub(r'\"registration_time\":.*?,\"category"', '\"registration_time\":{},\"category\"'.format(new_registration_time), curr_data)
     curr_data = re.sub(r'\"user_last_action_time\":.*?,\"user_birthdate"', '\"user_last_action_time\":{},\"user_birthdate\"'.format(new_user_last_action_time), curr_data)
 
+    
     session.execute(
         """
         INSERT INTO fashion (date, t, record) 
-        VALUES (%s, %s, %s)
+        VALUES (%s, %s, %s) USING TTL 604800
         """,
         ("{:04d}-{:02d}-{:02d}".format(ts_now.year, ts_now.month, ts_now.day), new_time, curr_data[:-1])
     )
 
-    time.sleep(1)
+    time.sleep(tdeltas[count % len(tdeltas)])
     count += 1
