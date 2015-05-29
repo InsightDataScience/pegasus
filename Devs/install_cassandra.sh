25d4cf782836a2a1914256d21da9879a456deacc
Austin Ouyang <AustinOuyang@Austins-Air.attlocal.net>
2015-05-29 01:12:19 -0700

    added cassandra installation

---
+++ Devs/install_cassandra.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# check input arguments
+if [ "$#" -ne 4 ]; then
+    echo "Please specify pem-key location, AWS region, AWS cluster name, and Cassandra cluster name" && exit 1
+fi
+
+PEMLOC=$1
+
+# check if pem-key location is valid
+if [ ! -f $PEMLOC ]; then
+    echo "pem-key does not exist!" && exit 1
+fi
+
+REGION=$2
+CLUSTERNAME=$3
+
+python fetch_instances.py $REGION $CLUSTERNAME
+
+cd Cassandra
+
+CASSANDRA_NAME=$4
+./setup_cassandra_cluster.sh $PEMLOC $CASSANDRA_NAME
