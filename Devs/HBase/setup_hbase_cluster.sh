049979fed29b48f734c06e5c5b1389548a4c5a7a
Austin Ouyang <AustinOuyang@Austins-Air.attlocal.net>
2015-06-15 23:57:25 -0700

    added HBase installation as well as Single node Redis

---
+++ Devs/HBase/setup_hbase_cluster.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# check input arguments
+if [ "$#" -ne 1 ]; then
+    echo "Please specify pem-key location!" && exit 1
+fi
+
+# get input arguments [aws region, pem-key location]
+PEMLOC=$1
+
+# check if pem-key location is valid
+if [ ! -f $PEMLOC ]; then
+    echo "pem-key does not exist!" && exit 1
+fi
+
+# import AWS private DNS's
+HOSTIP=()
+while read line; do
+    HOSTIP+=($line)
+done < ../private_dns
+
+# import AWS public DNS's
+DNS=()
+while read line; do
+    DNS+=($line)
+done < ../public_dns
+
+MASTER_DNS=$(head -n 1 ../public_dns)
+
+# Install HBase on master and slaves
+for dns in "${DNS[@]}"
+do
+    ssh -o "StrictHostKeyChecking no" -i $PEMLOC ubuntu@$dns 'bash -s' < setup_hbase.sh $MASTER_DNS "${HOSTIP[@]}" &
+done
+
+wait
+
+ssh -i $PEMLOC ubuntu@$MASTER_DNS 'start-hbase.sh'
+
+echo "HBase setup complete!"
