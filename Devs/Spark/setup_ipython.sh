5f0668a8c552fb57c2e6d39622b9781a53e6ab4f
Austin Ouyang <AustinOuyang@Austins-Air.attlocal.net>
2015-07-25 14:14:04 -0700

    maximized driver memory. Add scipy, pymc, nose to all nodes.

--- Devs/Spark/setup_ipython.sh
+++ Devs/Spark/setup_ipython.sh
@@ -15,4 +15,4 @@ EXECMEM=$(echo "0.90 * ($TOTMEM - 1000)" | bc -l)
 
 tmux new-session -s ipython_notebook -n bash -d
 
-tmux send-keys -t ipython_notebook 'PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777" pyspark --packages com.databricks:spark-csv_2.10:1.1.0 --master spark://'$(hostname)':7077 --executor-memory '${EXECMEM%.*}'M' C-m
+tmux send-keys -t ipython_notebook 'PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777" pyspark --packages com.databricks:spark-csv_2.10:1.1.0 --master spark://'$(hostname)':7077 --executor-memory '${EXECMEM%.*}'M --driver-memory '${EXECMEM%.*}'M' C-m
--- Devs/Spark/setup_single.sh
+++ Devs/Spark/setup_single.sh
@@ -2,7 +2,11 @@
 
 sudo apt-get update
 
-sudo apt-get --yes --force-yes install openjdk-7-jdk scala python-dev python-pip python-numpy
+sudo apt-get --yes --force-yes install openjdk-7-jdk scala python-dev python-pip python-numpy python-scipy gfortran
+
+sudo pip install nose
+
+sudo easy_install pymc
 
 wget https://dl.bintray.com/sbt/debian/sbt-0.13.7.deb -P ~/Downloads
 sudo dpkg -i ~/Downloads/sbt-*
