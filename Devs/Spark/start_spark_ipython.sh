dc916c7c3a2a3229e0ea0a025b80cd70567dd411
aouyang1 <aouyang1@gmail.com>
2015-10-12 16:35:46 +0900

    missed tmp reference in jupyter installation

--- Devs/Spark/start_spark_ipython.sh
+++ Devs/Spark/start_spark_ipython.sh
@@ -10,11 +10,11 @@ INSTANCE_NAME=$2
 AWS_ACCESS_KEY_ID=$3
 AWS_SECRET_ACCESS_KEY=$4
 
-ssh -o "StrictHostKeyChecking no" -i $PEMLOC ubuntu@$(head -n 1 $INSTANCE_NAME/public_dns) 'bash -s' < Spark/setup_ipython.sh $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
+ssh -o "StrictHostKeyChecking no" -i $PEMLOC ubuntu@$(head -n 1 tmp/$INSTANCE_NAME/public_dns) 'bash -s' < Spark/setup_ipython.sh $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
 
 sleep 3
 
-ssh -N -f -L localhost:7777:localhost:7777 ubuntu@$(head -n 1 $INSTANCE_NAME/public_dns)
+ssh -N -f -L localhost:7777:localhost:7777 ubuntu@$(head -n 1 tmp/$INSTANCE_NAME/public_dns)
 
 echo "IPython server is running at localhost:7777!"
 
