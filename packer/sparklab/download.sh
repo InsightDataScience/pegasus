7bd9037856788a9f2295d6afb7c66e7aa0c01658
aouyang1 <aouyang1@gmail.com>
2015-12-06 16:00:31 -0500

    added packer download script for us-east-1. Changed Zeppelin build back to Spark 1.4.1

---
+++ packer/sparklab/download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y install git
+git clone https://github.com/InsightDataScience/pegasus.git
+
+pegasus/install/Environment/install_env.sh
+pegasus/install/download_tech hadoop
+pegasus/install/download_tech hive
+pegasus/install/download_tech pig
+pegasus/install/download_tech spark
+pegasus/install/download_tech tachyon
+pegasus/Zeppelin/install_zeppelin.sh
