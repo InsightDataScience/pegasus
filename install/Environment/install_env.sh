175e1dbbc286af608cad4d0379720dfe9514f996
aouyang1 <aouyang1@gmail.com>
2015-12-10 23:55:16 -0500

    tested batch spark-hadoop deployment with 20 nodes. Adde scikit learn to environment installation

--- install/Environment/install_env.sh
+++ install/Environment/install_env.sh
@@ -14,7 +14,7 @@ sudo apt-add-repository -y ppa:andrei-pozolotin/maven3
 sudo apt-get update
 sudo apt-get --yes --force-yes install maven3
 
-sudo pip install nose seaborn boto "ipython[notebook]"
+sudo pip install nose seaborn boto scikit-learn "ipython[notebook]"
 
 if ! grep "export JAVA_HOME" ~/.profile; then
   echo -e "\nexport JAVA_HOME=/usr" | cat >> ~/.profile
