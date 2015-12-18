975059db86b209b047b0f39ffe2b44cfa503f8d6
aouyang1 <aouyang1@gmail.com>
2015-12-18 16:10:57 -0600

    added basic tests for InstanceConfig class

--- tests/test_boto_util.py
+++ tests/test_boto_util.py
@@ -2,12 +2,64 @@ from util.boto_util import InstanceConfig
 
 class TestInstanceConfig:
 
-    def test_bdm_vol_size_int(self):
+    def test_is_valid_single_correct_key(self):
         params = {"vol_size": 100}
 
         inst_conf = InstanceConfig(params)
-        bdm = inst_conf.create_block_device_map()
 
-        assert bdm['Ebs']['VolumeSize'] == 100
+        assert inst_conf.is_valid() == False
 
+    def test_is_valid_correct_keys_str(self):
+        params = {'region': 'us-west-2',
+                  'subnet': 'subnet-3a78835f',
+                  'purchase_type': 'on_demand',
+                  'image': 'ami-5189a661',
+                  'price': '0.25',
+                  'num_instance': 4,
+                  'key_name': 'instance-cluster',
+                  'security_group_ids': ['sg-9206aaf7'],
+                  'instance_type': 'm4x.large',
+                  'tag_name': 'test-cluster',
+                  'vol_size': 100,
+                  'bdm': {}
+                  }
+
+        inst_conf = InstanceConfig(params)
+
+        assert inst_conf.is_valid() == False
+
+    def test_is_valid_correct_keys_unicode(self):
+        params = {'region': u'us-west-2',
+                  'subnet': u'subnet-3a78835f',
+                  'purchase_type': u'on_demand',
+                  'image': u'ami-5189a661',
+                  'price': u'0.25',
+                  'num_instances': 4,
+                  'key_name': u'instance-cluster',
+                  'security_group_ids': [u'sg-9206aaf7'],
+                  'instance_type': u'm4x.large',
+                  'tag_name': u'test-cluster',
+                  'vol_size': 100
+                  }
+
+        inst_conf = InstanceConfig(params)
+
+        assert inst_conf.is_valid() == True
+
+    def test_is_valid_missing_a_key(self):
+        params = {'region': u'us-west-2',
+                  'subnet': u'subnet-3a78835f',
+                  'purchase_type': u'on_demand',
+                  'image': u'ami-5189a661',
+                  'price': u'0.25',
+                  'key_name': u'instance-cluster',
+                  'security_group_ids': [u'sg-9206aaf7'],
+                  'instance_type': u'm4x.large',
+                  'tag_name': u'test-cluster',
+                  'vol_size':100
+                  }
+
+        inst_conf = InstanceConfig(params)
+
+        assert inst_conf.is_valid() == False
 
