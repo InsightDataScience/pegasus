7b0b54c1adcef4494032ba3a6ccffdac46ca3a23
aouyang1 <aouyang1@gmail.com>
2016-01-05 11:12:09 -0800

    changed folders in config to lower case

--- config/Presto/config_worker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. ~/.profile
-
-# config.properties
-cat >> $PRESTO_HOME/etc/config.properties << EOL
-coordinator=false
-EOL
-
--- config/Storm/start_master.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. ~/.profile
-
-tmux new-session -s nimbus -n bash -d
-tmux send-keys -t nimbus '$STORM_HOME/bin/storm nimbus' C-m
-
-tmux new-session -s stormui -n bash -d
-tmux send-keys -t stormui '$STORM_HOME/bin/storm ui' C-m
