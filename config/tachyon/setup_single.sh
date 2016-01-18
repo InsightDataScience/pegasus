92790253f704e11ed03a0406e33b985a6f8ea63d
aouyang1 <aouyang1@gmail.com>
2016-01-18 01:26:28 -0800

    tachyon config has changed and still needs fixing. zookeeper repo updated to 3.4.6. added a requirements.txt. removed setup.py

--- config/tachyon/setup_single.sh
+++ config/tachyon/setup_single.sh
@@ -6,7 +6,9 @@ WORKER_HOSTNAMES=( "$@" )
 . ~/.profile
 
 cp $TACHYON_HOME/conf/tachyon-env.sh.template $TACHYON_HOME/conf/tachyon-env.sh
-sed -i 's@TACHYON_MASTER_ADDRESS=localhost@TACHYON_MASTER_ADDRESS='"$MASTER_HOSTNAME"'@g' $TACHYON_HOME/conf/tachyon-env.sh
+
+echo "export TACHYON_MASTER_ADDRESS=$MASTER_HOSTNAME" | cat >> ~/.profile
+. ~/.profile
 
 mv $TACHYON_HOME/conf/workers $TACHYON_HOME/conf/workers.backup
 for worker in ${WORKER_HOSTNAMES[@]}
