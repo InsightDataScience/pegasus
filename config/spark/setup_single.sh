eeaf328dce454e0a3be65ed6dd2fec5cc0970b28
aouyang1 <aouyang1@gmail.com>
2016-03-28 02:14:49 -0700

    added s3a access key for hadoop. Spark now depends on hadoop installation for aws jar files. Ipython for spark is published on default port 8888 to follow standard out of the box

--- config/spark/setup_single.sh
+++ config/spark/setup_single.sh
@@ -2,10 +2,24 @@
 
 . ~/.profile
 
-cp $SPARK_HOME/conf/spark-env.sh.template $SPARK_HOME/conf/spark-env.sh
+cp ${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-*.jar ${SPARK_HOME}/lib
+cp ${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-*.jar ${SPARK_HOME}/lib
 
+cp ${SPARK_HOME}/conf/spark-env.sh.template ${SPARK_HOME}/conf/spark-env.sh
+cp ${SPARK_HOME}/conf/spark-defaults.conf.template ${SPARK_HOME}/conf/spark-defaults.conf
+
+# configure spark-env.sh
 OVERSUBSCRIPTION_FACTOR=3
-WORKER_CORES=$(echo "$(nproc)*$OVERSUBSCRIPTION_FACTOR" | bc)
-sed -i '6i export JAVA_HOME=/usr' $SPARK_HOME/conf/spark-env.sh
-sed -i '7i export SPARK_PUBLIC_DNS="'$1'"' $SPARK_HOME/conf/spark-env.sh
-sed -i '8i export SPARK_WORKER_CORES='$WORKER_CORES'' $SPARK_HOME/conf/spark-env.sh
+WORKER_CORES=$(echo "$(nproc)*${OVERSUBSCRIPTION_FACTOR}" | bc)
+sed -i '6i export JAVA_HOME=/usr' ${SPARK_HOME}/conf/spark-env.sh
+sed -i '7i export SPARK_PUBLIC_DNS="'$1'"' ${SPARK_HOME}/conf/spark-env.sh
+sed -i '8i export SPARK_WORKER_CORES='${WORKER_CORES}'' ${SPARK_HOME}/conf/spark-env.sh
+sed -i '9i export DEFAULT_HADOOP_HOME='${HADOOP_HOME}'' ${SPARK_HOME}/conf/spark-env.sh
+
+
+# configure spark-defaults.conf
+hadoop_aws_jar=$(find /usr/local/spark/lib -type f | grep hadoop-aws)
+aws_java_sdk_jar=$(find /usr/local/spark/lib -type f | grep aws-java-sdk)
+sed -i '21i spark.hadoop.fs.s3a.impl org.apache.hadoop.fs.s3a.S3AFileSystem' ${SPARK_HOME}/conf/spark-defaults.conf
+sed -i '22i spark.executor.extraClassPath '"${aws_java_sdk_jar}"':'"${hadoop_aws_jar}"'' ${SPARK_HOME}/conf/spark-defaults.conf
+sed -i '23i spark.driver.extraClassPath '"${aws_java_sdk_jar}"':'"${hadoop_aws_jar}"'' ${SPARK_HOME}/conf/spark-defaults.conf
--- examples/spark_hadoop.sh
+++ examples/spark_hadoop.sh
@@ -13,6 +13,7 @@ peg fetch ${CLUSTER_NAME}
 
 peg install ${CLUSTER_NAME} ssh
 peg install ${CLUSTER_NAME} aws
+peg install ${CLUSTER_NAME} hadoop
 peg install ${CLUSTER_NAME} spark
 
-peg port-forward ${CLUSTER_NAME} 1 7777:7777
+peg port-forward ${CLUSTER_NAME} 1 8888:8888
