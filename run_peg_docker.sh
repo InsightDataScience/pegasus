5ad23cdba42cc42a02decf0950dd89a65fda02b4
aouyang1 <aouyang1@gmail.com>
2016-04-30 18:55:04 -0400

    updated README and run script for docker

--- run_peg_docker.sh
+++ run_peg_docker.sh
@@ -4,7 +4,7 @@ nargs="$#"
 
 if [ "${nargs}" -ne 2 ]; then
   echo "incorrect number of arguments"
-  echo "run.sh <pem-key-name> <instance-template-folder>"
+  echo "./run_peg_docker.sh <pem-key-name> <instance-template-folder>"
   exit 1
 fi
 
@@ -25,8 +25,9 @@ folder_split=($(echo ${instance_template_folder} | tr "/" " "))
 folder_name=${folder_split[${#folder_split[@]}-1]}
 
 docker run -it --rm --name peg \
-  -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:?"set AWS_ACCESS_KEY_ID"} \
-  -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:?"set AWS_SECRET_ACCESS_KEY"} \
+  -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:?"set AWS_ACCESS_KEY_ID before proceeding"} \
+  -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:?"set AWS_SECRET_ACCESS_KEY before proceeding"} \
+  -e AWS_DEFAULT_REGION=${AWS_DEFUALT_REGION:?"set AWS_DEFAULT_REGION before proceeding"} \
   -e USER=${USER:=pegasus} \
   -v ~/.ssh/${pem_key_name}.pem:/root/.ssh/${pem_key_name}.pem \
   -v ${instance_template_folder}:/root/${folder_name} \
