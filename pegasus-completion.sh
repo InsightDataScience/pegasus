2aa2d29bbee3f824921c48a3ccc56007d223bde8
Ronak Nathani <ronaknnathani@gmail.com>
2016-05-25 00:19:02 -0700

    added usage comments

--- pegasus-completion.sh
+++ pegasus-completion.sh
@@ -1,6 +1,23 @@
 # Bash completion for peg commands
 # Usage: Put "source pegasus-completion.sh" into your .bash_profile (on mac) or .bashrc (on linux)
-# If you are using bash_completion, just place this script 
+# 
+# OR
+#
+# If you are using bash_completion, 
+#    for linux
+#           place this script under /etc/bash_completion.d folder (for linux)
+#    and add the following lines to your .profile
+#    if [ -f /etc/bash_completion ]; then 
+#        . /etc/bash_completion 
+#    fi
+#
+#    for mac
+#           place this script under `brew --prefix`/etc/bash_completion.d (for mac)
+#    and add the following lines to your .bash_profile
+#    if [ -f `brew --prefix`/etc/bash_completion ]; then 
+#        . `brew --prefix`/etc/bash_completion 
+#    fi
+
 _peg() 
 {
     local cur prev opts
