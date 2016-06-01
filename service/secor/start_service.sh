70cdd0dd9b7a6f7ee68436558d30a07694537fa1
Ronak Nathani <ronaknnathani@gmail.com>
2016-06-01 15:33:13 -0700

    updated start and stop scripts

--- service/secor/start_service.sh
+++ service/secor/start_service.sh
@@ -10,10 +10,10 @@ fi
 CLUSTER_NAME=$1
 
 MASTER_PUBLIC_DNS=$(fetch_public_dns_of_node_in_cluster ${CLUSTER_NAME} 1)
+script=${PEG_ROOT}/service/secor/start_secor.sh
 
-cmd='cd $SECOR_HOME/bin; java -ea -Dsecor_group=secor_backup -Dlog4j.configuration=log4j.prod.properties -Dconfig=secor.prod.backup.properties -cp secor-0.21-SNAPSHOT.jar:lib/* com.pinterest.secor.main.ConsumerMain'
+cmd='source ~/.profile; cd /usr/local/secor/bin; pwd; ls; sudo java -ea -Dsecor_group=secor_backup -Dlog4j.configuration=log4j.prod.properties -Dconfig=secor.prod.backup.properties -cp secor-0.21-SNAPSHOT.jar:lib/* com.pinterest.secor.main.ConsumerMain'
 
-# Start secor on Master Node 
-run_cmd_on_node ${MASTER_PUBLIC_DNS} ${cmd} &
+run_cmd_on_node ${MASTER_PUBLIC_DNS} ${cmd}
 
 echo "Secor Started!"
