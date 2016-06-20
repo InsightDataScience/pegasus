315a34c45cebd8aaec3af613d7c7f9f64ab6348a
aouyang1 <aouyang1@gmail.com>
2016-06-19 18:27:10 -0700

    removed service start after installation. Added hadoop dependency for flink to read from s3. removed webclient start in flink since it's been deprecated

--- service/flink/stop_service.sh
+++ service/flink/stop_service.sh
@@ -11,7 +11,6 @@ CLUSTER_NAME=$1
 
 MASTER_DNS=$(fetch_cluster_master_public_dns ${CLUSTER_NAME})
 
-run_cmd_on_node ${MASTER_DNS} '/usr/local/flink/bin/stop-webclient.sh'
 run_cmd_on_node ${MASTER_DNS} '/usr/local/flink/bin/stop-cluster.sh'
 
 echo "Flink Stopped!"
