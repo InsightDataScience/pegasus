c9ce46220ad2e3b57d26cac1057dc505ee993551
Ronak Nathani <ronaknnathani@gmail.com>
2016-06-20 15:25:13 -0700

    added minimum number of masters in elasticsearch.yml for ES cluster to avoid split brain problem

--- config/elasticsearch/setup_cluster.sh
+++ config/elasticsearch/setup_cluster.sh
@@ -11,9 +11,11 @@ source ${PEG_ROOT}/util.sh
 CLUSTER_NAME=$1
 
 PUBLIC_DNS=$(fetch_cluster_public_dns ${CLUSTER_NAME})
+NUMBER_OF_NODES=$(wc -l < ${PEG_ROOT}/tmp/CLUSTER_NAME/public_dns)
+QUORUM=$((${NUMBER_OF_NODES}/2 + 1))
 
 single_script="${PEG_ROOT}/config/elasticsearch/setup_single.sh"
-args="$CLUSTER_NAME $AWS_DEFAULT_REGION $AWS_SECRET_ACCESS_KEY $AWS_ACCESS_KEY_ID"
+args="$CLUSTER_NAME $AWS_DEFAULT_REGION $AWS_SECRET_ACCESS_KEY $AWS_ACCESS_KEY_ID $QUORUM"
 # Install and configure nodes for elasticsearch
 for dns in ${PUBLIC_DNS}; do
   run_script_on_node ${dns} ${single_script} ${args} &
--- config/elasticsearch/setup_single.sh
+++ config/elasticsearch/setup_single.sh
@@ -5,6 +5,7 @@ ES_NAME=$1
 AWS_REGION=$2
 AWS_SECRET=$3
 AWS_ACCESS=$4
+QUORUM=$5
 
 . ~/.profile
 
@@ -21,5 +22,7 @@ sudo sed -i '1i cloud.aws.region: '"$AWS_REGION"'' $ELASTICSEARCH_HOME/config/el
 sudo sed -i '1i cloud.aws.secret_key: '"$AWS_SECRET"'' $ELASTICSEARCH_HOME/config/elasticsearch.yml
 sudo sed -i '1i cloud.aws.access_key: '"$AWS_ACCESS"'' $ELASTICSEARCH_HOME/config/elasticsearch.yml
 sudo sed -i '1i network.host: 0.0.0.0' $ELASTICSEARCH_HOME/config/elasticsearch.yml
+sudo sed -i '1i discovery.zen.minimum_master_nodes: '"$QUORUM"'' $ELASTICSEARCH_HOME/config/elasticsearch.yml
+# discovery.zen.minimum_master_nodes: 1
 
 sudo chown -R ubuntu $ELASTICSEARCH_HOME
