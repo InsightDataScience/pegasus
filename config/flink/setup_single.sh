d2992913bf2f679bb348281ed24f7577b84689ab
aouyang1 <aouyang1@gmail.com>
2016-06-19 23:58:13 -0700

    changing task slots to 2 or 3x of cpus has no effect on computation speed

--- config/flink/setup_single.sh
+++ config/flink/setup_single.sh
@@ -8,7 +8,8 @@ NUM_WORKERS=$2
 MEMINFO=($(free -m | sed -n '2p' | sed -e "s/[[:space:]]\+/ /g"))
 TOTMEM=${MEMINFO[1]}
 TASKMANAGER_HEAP=$(printf "%.0f" $(echo "0.90 * ($TOTMEM - 1000)" | bc -l))
-PARALLELISM=$(echo "$(nproc) * $NUM_WORKERS" | bc -l)
+TASK_SLOTS=$(nproc)
+PARALLELISM=$(echo "$TASK_SLOTS * $NUM_WORKERS" | bc -l)
 TMP_DIRS=/var/flink/tmp
 
 sudo mkdir -p $TMP_DIRS
@@ -22,7 +23,7 @@ cp ${HADOOP_HOME}/share/hadoop/tools/lib/httpcore-*.jar ${FLINK_HOME}/lib
 sed -i "s@jobmanager.rpc.address: localhost@jobmanager.rpc.address: $MASTER_IP@g" $FLINK_HOME/conf/flink-conf.yaml
 sed -i "s@jobmanager.heap.mb: 256@jobmanager.heap.mb: 1024@g" $FLINK_HOME/conf/flink-conf.yaml
 sed -i "s@taskmanager.heap.mb: 512@taskmanager.heap.mb: $TASKMANAGER_HEAP@g" $FLINK_HOME/conf/flink-conf.yaml
-sed -i "s@taskmanager.numberOfTaskSlots: 1@taskmanager.numberOfTaskSlots: $(nproc)@g" $FLINK_HOME/conf/flink-conf.yaml
+sed -i "s@taskmanager.numberOfTaskSlots: 1@taskmanager.numberOfTaskSlots: $TASK_SLOTS@g" $FLINK_HOME/conf/flink-conf.yaml
 sed -i "s@parallelism.default: 1@parallelism.default: $PARALLELISM@g" $FLINK_HOME/conf/flink-conf.yaml
 sed -i "s@# taskmanager.tmp.dirs: /tmp@taskmanager.tmp.dirs: $TMP_DIRS@g" $FLINK_HOME/conf/flink-conf.yaml
 sed -i "s@# fs.hdfs.hadoopconf: /path/to/hadoop/conf/@fs.hdfs.hadoopconf: $HADOOP_HOME/etc/hadoop@g" $FLINK_HOME/conf/flink-conf.yaml
