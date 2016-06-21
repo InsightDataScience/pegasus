29250d5e380719f45edd50515c48a19050117a43
Ronak Nathani <ronaknnathani@gmail.com>
2016-06-20 23:32:01 -0700

    removed stratup line at the end of kibana installation

--- config/kibana/setup_single.sh
+++ config/kibana/setup_single.sh
@@ -7,5 +7,3 @@ ELASTICSEARCH_DNS=$1
 sudo $KIBANA_HOME/bin/kibana plugin --install elasticsearch/marvel/2.3.3
 
 sed -i 's@elasticsearch_url: "localhost:9200"@elasticsearch_url: "'"$ELASTICSEARCH_DNS"':9200"@g' $KIBANA_HOME/config/kibana.yml
-
-sudo $KIBANA_HOME/bin/kibana &
