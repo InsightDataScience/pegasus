6998803d2051b4e7fb5be62cf9273c845877bbfb
aouyang1 <aouyang1@gmail.com>
2016-08-30 11:55:10 -0700

    refined memsql uninstall and added Apache License 2.0

--- pegasus-completion.sh
+++ pegasus-completion.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Copyright 2015 Insight Data Science
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Bash completion for peg commands
 # Usage:
 #     - Export PEGASUS_HOME as an environment variable as follows - in your .bash_profile (for mac) and .bashrc (for linux)
@@ -21,7 +37,7 @@
 #        . `brew --prefix`/etc/bash_completion 
 #    fi
 
-_peg() 
+_peg()
 {
     local cur prev opts
     COMPREPLY=()
@@ -58,6 +74,3 @@ _peg()
 complete -F _peg peg
 
 # END peg completion
-
-
- 	  	 
